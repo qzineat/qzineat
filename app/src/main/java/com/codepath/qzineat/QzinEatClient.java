0113b2bd64d6116576145a6d27a4a641d90f0e6e
Rokde <shyam.rokde@gmail.com>
2016-03-02 00:38:29 -0800

    added mock event json

---
+++ app/src/main/java/com/codepath/qzineat/QzinEatClient.java
@@ -0,0 +1,36 @@
+package com.codepath.qzineat;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * Created by Shyam Rokde on 3/2/16.
+ *
+ * All APIs for QZinEat App
+ */
+
+public class QzinEatClient {
+
+  private static final String API_BASE_URL = "https://raw.githubusercontent.com/qzineat/qzineat/";
+
+  private AsyncHttpClient client;
+
+  public QzinEatClient(){
+    client = new AsyncHttpClient();
+  }
+
+  private String getApiUrl(String relativeUrl) {
+    return API_BASE_URL + relativeUrl;
+  }
+
+  /**
+   * Get Events
+   * TODO: We can change this later for getting events based on "query"
+   */
+  public void getEvents(AsyncHttpResponseHandler handler){
+    RequestParams params = new RequestParams();
+
+    client.get(getApiUrl("eventlist.json"), params, handler);
+  }
+}
